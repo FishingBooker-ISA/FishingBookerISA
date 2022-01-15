6730f55e1104759e49499c9b60875257624ad420
anagrahovac <grahovac.ana99@gmail.com>
2022-01-15 22:31:53 +0100

    feat: implement filter and sort estates

--- FishingBookerWebApp/src/app/components/estates/estates.component.ts
+++ FishingBookerWebApp/src/app/components/estates/estates.component.ts
@@ -10,14 +10,16 @@ import { SignupOwnersService } from 'src/app/services/signup-owners.service';
   styleUrls: ['./estates.component.css']
 })
 export class EstatesComponent implements OnInit {
+  public availableEstates: DisplayServiceShortDTO[] = [];
   public backupEstates: DisplayServiceShortDTO[] = [];
   public estates: DisplayServiceShortDTO[] = [];
+  public searched: DisplayServiceShortDTO[] = [];
   
   public searchText: string = "";
   public searchCriteria: string = "name";
   public ratingFrom = 0;
   public ratingTo = 5;
-  public location: string = "";
+  public location = "any";
   public sortOrder = "asc";
   public sortCriteria = "";
   currentUser!: User
@@ -39,7 +41,7 @@ export class EstatesComponent implements OnInit {
   }
 
   getAllEstates() {
-    this._estateService.getAllEstates().subscribe((data) => this.estates = data)
+    this._estateService.getAllEstates().subscribe((data) => {this.estates = data; this.backupEstates = Array.from(data);})
   }
 
   search(){
@@ -52,18 +54,74 @@ export class EstatesComponent implements OnInit {
     
     this.ratingFrom = 0;
     this.ratingTo = 5;
-    this.location= "";
+    this.location = "any";
     this.sortOrder = "asc";
     this.sortCriteria = "";
     this.applyFilterAndSort();
   }
-  applyFilterAndSort() {
+
+  applyFilterAndSort(){
+    this.applyFilter();
+    
+    this.applySort();
   }
+
+  applySort() {
+    switch(this.sortCriteria){
+      case "name":
+      {
+        if(this.sortOrder == "asc")
+          this.estates = this.estates.sort((a, b) => (a.name > b.name ? 1 : -1));
+        else
+          this.estates = this.estates.sort((a, b) => (a.name > b.name ? -1 : 1));
+        break;
+      }
+      case "city":
+      {
+        if(this.sortOrder == "asc")
+          this.estates = this.estates.sort((a, b) => (a.address.city > b.address.city ? 1 : -1));
+        else
+          this.estates = this.estates.sort((a, b) => (a.address.city > b.address.city ? -1 : 1));
+        break;
+      }
+      case "rating":
+      {
+        if(this.sortOrder == "asc")
+          this.estates = this.estates.sort((a, b) => (a.rating > b.rating ? 1 : -1));
+        else
+          this.estates = this.estates.sort((a, b) => (a.rating > b.rating ? -1 : 1));
+        break;
+      }
+      case "price":
+      {
+        if(this.sortOrder == "asc")
+          this.estates = this.estates.sort((a, b) => (a.pricePerDay > b.pricePerDay ? 1 : -1));
+        else
+          this.estates = this.estates.sort((a, b) => (a.pricePerDay > b.pricePerDay ? -1 : 1));
+        break;
+      }
+    }
+  }
+
+  applyFilter() {
+    this.estates = Array.from(this.backupEstates);
+    switch(this.location){
+      case "any": this.estates = this.estates.filter((e) => true); break;
+      default: this.estates = this.estates.filter((e) => e.address.city.toLowerCase() == this.location);
+    }
+    if(this.ratingFrom > 0){
+      this.estates = this.estates.filter((e) => e.rating >= this.ratingFrom); 
+    }
+    if(this.ratingTo < 5){
+      this.estates = this.estates.filter((e) => e.rating <= this.ratingTo); 
+    }
+  }
+
   searchByName(input: string) {
-    this._estateService.getEstatesByName(input).subscribe((data) => this.estates = data)
+    this._estateService.getEstatesByName(input).subscribe((data) => {this.estates = data; this.backupEstates = Array.from(data);})
   }
   searchByCity(input: string) {
-    this._estateService.getEstatesByCity(input).subscribe((data) => this.estates = data)
+    this._estateService.getEstatesByCity(input).subscribe((data) => {this.estates = data; this.backupEstates = Array.from(data);})
   }
 
 }
