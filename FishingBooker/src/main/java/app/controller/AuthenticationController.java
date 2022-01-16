0476eeacf95a84550585a008d8512ec4c56d176f
jovicpetra <jovic.petra@uns.ac.rs>
2022-01-17 00:29:45 +0100

    fix: code smells fixed

--- FishingBooker/src/main/java/app/controller/AuthenticationController.java
+++ FishingBooker/src/main/java/app/controller/AuthenticationController.java
@@ -1,7 +1,5 @@
 package app.controller;
 
-import javax.servlet.http.HttpServletResponse;
-
 import app.domain.User;
 import app.dto.*;
 import app.service.ClientService;
@@ -17,8 +15,12 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.util.UriComponentsBuilder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
 
 //Kontroler zaduzen za autentifikaciju korisnika
 @RestController
--- FishingBooker/src/main/java/app/repository/ReportRepository.java
+++ FishingBooker/src/main/java/app/repository/ReportRepository.java
@@ -1,10 +1,7 @@
 package app.repository;
 
 import app.domain.Report;
-import app.domain.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
 
 public interface ReportRepository  extends JpaRepository<Report, Integer> {
     Report getByReservationId(int id);
